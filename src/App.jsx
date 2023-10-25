8483d9e75c0b6471ba4cfd9e923dd64b9fc80795
Esteban Pelutiero <estebanpelutiero@gmail.com>
2023-10-25 16:51:11 -0300

    icons added, footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <Nav />
+      <Footer />
     </>
   )
 }
